1299c110f223b979f49f898a875dd58de742de41
pongsapat-lab <pongsapat.piti@bumail.net>
2026-08-30 16:55:58 +0700

    Work p2

--- Lab02/Program.cs
+++ Lab02/Program.cs
@@ -41,6 +41,12 @@ namespace Lab02
             Console.WriteLine("===== BOSS STATUS: AFTER DAMAGE =====");
             Console.WriteLine("Hp: " + currentHp + "/" + maxHp);
             Console.WriteLine("Hp Percent: " + hpPercent+ "%");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+
 
             //----------------------------------------------------------------------------------------------
             //Part 2
@@ -50,12 +56,70 @@ namespace Lab02
             // Console.WriteLine($"Speed: {speed}");
             // ทำซ้ำแบบนี้ให้ครบ 4 ตัวละคร ตัวละ 5 stat
 
-            string Member1 = "John"
+            // 1
+            string Member1 = "John";
             int m_1_maxHp = 15;
-            int m_1_maxMana = 30;
-            float m_1_atk = 2.4f;
-            double m_1_crit = 0.05;
+            int m_1_level = 19;
+            float m_1_atk = 25.6f;
+            double m_1_crit = 25;
 
+            //2
+            string Member2 = "Lisa";
+            int m_2_maxHp = 45;
+            int m_2_level = 16;
+            float m_2_atk = 55.7f;
+            double m_2_crit = -1;
+
+            //3
+            string Member3 = "Rose";
+            int m_3_maxHp = 20;
+            int m_3_level = 14;
+            float m_3_atk = 7.1f;
+            double m_3_crit = 5;
+
+            //4
+            string miniBoss = "Raja";
+            char mini_Danger = 'B';
+            int mini_level = 20;
+            int mini_maxHp = 240;
+            float mini_atk = 12.5f;
+            double mini_crit = 7.25;
+            bool isminiBoss = true;
+
+
+            Console.WriteLine("WARNING! WARNING MINI BOSS AHEAD! WARNING! WARNING!");
+            Console.WriteLine($"Name: {miniBoss}");
+            Console.WriteLine($"Rank: {mini_Danger}");
+            Console.WriteLine($"Level: {mini_level}");
+            Console.WriteLine($"Max Hp: {maxHp}");
+            Console.WriteLine($"Attack Power: {mini_atk}");
+            Console.WriteLine($"Crit Multiplier: {mini_crit}");
+            Console.WriteLine($"Is Boss: {isminiBoss}");
+            Console.WriteLine();
+
+            Console.WriteLine("Parth Member");
+            Console.WriteLine($"Name: {Member1}");
+            Console.WriteLine($"Level:  {m_1_level}");
+            Console.WriteLine($"Max Hp: {m_1_maxHp}");
+            Console.WriteLine($"Attack Power: {m_1_atk}");
+            Console.WriteLine($"Crit Multiplier: {m_1_crit}");
+            Console.WriteLine();
+
+            Console.WriteLine($"Name: {Member1}");
+            Console.WriteLine($"Level:  {m_2_level}");
+            Console.WriteLine($"Max Hp: {m_2_maxHp}");
+            Console.WriteLine($"Attack Power: {m_2_atk}");
+            Console.WriteLine($"Crit Multiplier: {m_2_crit}");
+            Console.WriteLine();
+
+            Console.WriteLine($"Name: {Member3}");
+            Console.WriteLine($"Level:  {m_3_level}");
+            Console.WriteLine($"Max Hp: {m_3_maxHp}");
+            Console.WriteLine($"Attack Power: {m_3_atk}");
+            Console.WriteLine($"Crit Multiplier: {m_3_crit}");
+            Console.WriteLine();
+
+            Console.WriteLine("     ENGAGE!");
 
 
 
